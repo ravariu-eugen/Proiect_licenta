09552f34659c0504df02b9fb13380d3672f62d95
ravariu-eugen <74059247+ravariu-eugen@users.noreply.github.com>
2024-07-13 10:41:39 +0300

    added BucketManagerTest

---
+++ src/test/java/proiect_licenta/planner/storage/BucketManagerTest.java
@@ -0,0 +1,39 @@
+package proiect_licenta.planner.storage;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BucketManagerTest {
+	private BucketStorage bucketManager;
+	@BeforeEach
+	void setUp() {
+
+
+	}
+
+	@Test
+	void bucketExists() {
+
+
+	}
+
+	@Test
+	void listObjects() {
+	}
+
+	@Test
+	void objectExists() {
+	}
+
+	@Test
+	void uploadFile() {
+	}
+
+	@Test
+	void downloadFile() {
+	}
+
+	@Test
+	void deleteFile() {
+	}
+}
