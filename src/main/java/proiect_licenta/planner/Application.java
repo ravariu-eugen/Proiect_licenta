124de2187b90f665d5f9b4a4fa95ac46f1caf959
ravariu-eugen <74059247+ravariu-eugen@users.noreply.github.com>
2024-07-13 10:43:45 +0300

    modified Application

--- src/main/java/proiect_licenta/planner/Application.java
+++ src/main/java/proiect_licenta/planner/Application.java
@@ -4,6 +4,13 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import proiect_licenta.planner.storage.BucketStorage;
+import proiect_licenta.planner.helper.Helper;
+import proiect_licenta.planner.jobs.JobList;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @SpringBootApplication
 public class Application {
@@ -12,7 +19,37 @@ public class Application {
 
 
 	public static void main(String[] args) {
-		  SpringApplication.run(Application.class, args);
+
+
+
+//		String jobListJSON = Helper.getResourceAsString("joblists/joblist1.json");
+//		JobList jobList = JobList.createJobList(jobListJSON);
+//		jobList.getJobs().forEach(job -> {
+//			logger.info(job.toString());
+//		});
+//		BucketStorage bucketManager = new BucketStorage(Helper.getBucketName());
+//		logger.info(bucketManager.listObjects());
+//		bucketManager.downloadFile("numbers1.txt", "numbers.txt");
+//
+//
+//		// read file
+//		try {
+//			String content = Files.readString(Paths.get("numbers1.txt"));
+//			logger.info(content);
+//		} catch (IOException e) {
+//			throw new RuntimeException(e);
+//		}
+//
+//		bucketManager.uploadFile("numbers1.txt", "numbers1.txt");
+//		logger.info(bucketManager.listObjects());
+//		boolean deleted = bucketManager.deleteObject("numbers1.txt");
+//		logger.info(deleted);
+//		logger.info(bucketManager.listObjects());
+//		deleted = bucketManager.deleteObject("numbs1.txt");
+//		logger.info(deleted);
+//		logger.info(bucketManager.listObjects());
+//		logger.info(bucketManager.objectExists("numbers.txt"));
+		SpringApplication.run(Application.class, args);
 	}
 
 
