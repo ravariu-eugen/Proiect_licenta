4ca10ecc85ae1b228405e2c582bc73ed5bb11685
ravariu-eugen <74059247+ravariu-eugen@users.noreply.github.com>
2024-07-13 10:40:57 +0300

    added builder

--- src/main/java/proiect_licenta/planner/jobs/CopyJob.java
+++ src/main/java/proiect_licenta/planner/jobs/CopyJob.java
@@ -1,4 +1,74 @@
 package proiect_licenta.planner.jobs;
 
-public class CopyJob {
+import org.jetbrains.annotations.NotNull;
+import proiect_licenta.planner.storage.BucketStorage;
+import proiect_licenta.planner.helper.Helper;
+import proiect_licenta.planner.storage.StorageInterface;
+import software.amazon.awssdk.services.s3.model.CopyObjectResponse;
+
+import java.util.List;
+import java.util.Map;
+import java.util.StringJoiner;
+import java.util.concurrent.CompletableFuture;
+
+public class CopyJob extends Job {
+	private final String inputDataSet;
+	private final String outputDataSet;
+	private final StorageInterface storage;
+	public CopyJob(String name, String description, String inputDataSet, String outputDataSet) {
+		super(name, description);
+		this.inputDataSet = inputDataSet;
+		this.outputDataSet = outputDataSet;
+
+		this.storage = new BucketStorage(Helper.getBucketName());
+	}
+	public static @NotNull CopyJob builder(@NotNull Map<String, Object> jobMap) {
+
+		String name = (String) jobMap.get("name");
+		if (name == null) throw new IllegalArgumentException("Job name cannot be null");
+
+		String description = (String) jobMap.get("description");
+
+		String inputDataSet = (String) jobMap.get("inputDataSet");
+		if (inputDataSet == null) throw new IllegalArgumentException("Input data set cannot be null");
+
+		String outputDataSet = (String) jobMap.get("outputDataSet");
+		if (outputDataSet == null) throw new IllegalArgumentException("Output data set cannot be null");
+
+		return new CopyJob(name, description, inputDataSet, outputDataSet);
+	}
+	@Override
+	public JobType getJobType() {
+		return JobType.COPY;
+	}
+
+	@Override
+	public List<String> getDependencies() {
+		return List.of(inputDataSet);
+	}
+
+	@Override
+	public List<String> getOutputs() {
+		return List.of(outputDataSet);
+	}
+
+	@Override
+	public void launch() {
+		boolean response = storage.copy(inputDataSet, outputDataSet);
+	}
+
+	@Override
+	public void waitUntilFinished() {
+
+	}
+
+	@Override
+	public String toString() {
+		return new StringJoiner(", ", CopyJob.class.getSimpleName() + "[", "]")
+				.add("name='" + name + "'")
+				.add("description='" + description + "'")
+				.add("inputDataSet='" + inputDataSet + "'")
+				.add("outputDataSet='" + outputDataSet + "'")
+				.toString();
+	}
 }
--- src/main/java/proiect_licenta/planner/jobs/DeleteJob.java
+++ src/main/java/proiect_licenta/planner/jobs/DeleteJob.java
@@ -1,15 +1,36 @@
 package proiect_licenta.planner.jobs;
 
+import org.jetbrains.annotations.NotNull;
+import proiect_licenta.planner.storage.BucketStorage;
+import proiect_licenta.planner.helper.Helper;
+
 import java.util.List;
+import java.util.Map;
+import java.util.StringJoiner;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 public class DeleteJob extends Job {
 	private final String inputDataSet;
+	private final BucketStorage bucketManager;
 	public DeleteJob(String name, String description, String inputDataSet) {
 		super(name, description);
-
 		this.inputDataSet = inputDataSet;
+		this.bucketManager = new BucketStorage(Helper.getBucketName());
 	}
 
+	public static @NotNull DeleteJob builder(@NotNull Map<String, Object> jobMap) {
+
+		String name = (String) jobMap.get("name");
+		if (name == null) throw new IllegalArgumentException("Job name cannot be null");
+
+		String description = (String) jobMap.get("description");
+
+		String inputDataSet = (String) jobMap.get("inputDataSet");
+		if (inputDataSet == null) throw new IllegalArgumentException("Input data set cannot be null");
+
+		return new DeleteJob(name, description, inputDataSet);
+	}
 	@Override
 	public JobType getJobType() {
 		return JobType.DELETE;
@@ -23,5 +44,31 @@ public class DeleteJob extends Job {
 	@Override
 	public List<String> getOutputs() {
 		return List.of();
+	}
+	private Future<Boolean> future;
+	@Override
+	public void launch() {
+
+		future = bucketManager.deleteObject(inputDataSet);
+	}
+
+	@Override
+	public void waitUntilFinished() {
+		try {
+			future.get();
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		} catch (ExecutionException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return new StringJoiner(", ", DeleteJob.class.getSimpleName() + "[", "]")
+				.add("name='" + name + "'")
+				.add("description='" + description + "'")
+				.add("inputDataSet='" + inputDataSet + "'")
+				.toString();
 	}
 }
